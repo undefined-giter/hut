6cf5f12a7611c2ab8d7eefba722eb2312690e05f
Lysard <leo.ripert@gmail.com>
2024-11-19 18:11:45 +0100

    Fixed adminComment

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'authWithGoogle'   => 'Compte est lié à Google, connectez-vous avec Google ci-dessus.',
+    'authWithGoogle'   => 'Compte est lié à Google, connectez-vous avec Google 🔺',
     'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements.',
     'password' => 'Le mot de passe est incorrect',
     'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
