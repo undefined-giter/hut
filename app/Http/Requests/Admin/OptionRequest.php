6d163620f998b72cfe0d79f5cb304069a1fd27d7
Lysard <leo.ripert@gmail.com>
2024-11-19 18:21:25 +0100

    Fixing forgoted uses

--- app/Http/Requests/Admin/OptionRequest.php
+++ app/Http/Requests/Admin/OptionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Admin;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OptionRequest extends FormRequest
--- app/Http/Requests/Admin/PriceRequest.php
+++ app/Http/Requests/Admin/PriceRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Admin;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PriceRequest extends FormRequest
